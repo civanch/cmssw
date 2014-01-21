218b6e93d0adb3bd4d20ab4538e8b8437b2b972a
Vladimir <Vladimir.Ivantchenko@cern.ch>
2014-01-21 19:36:27 +0100

    added protection against abcence of g4SimHits

--- SimG4Core/Application/python/reproc2011_2012_cff.py
+++ SimG4Core/Application/python/reproc2011_2012_cff.py
@@ -2,6 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 def customiseG4(process):
 
+    process.load('SimG4Core.Application.g4SimHits_cfi')
+
     process.g4SimHits.Physics.type = cms.string('SimG4Core/Physics/QGSP_FTFP_BERT_EML_New')
 
     # use HF shower library instead of GFlash parameterization
@@ -10,5 +12,5 @@ def customiseG4(process):
     process.g4SimHits.HCalSD.UseParametrize = cms.bool(False)
     process.g4SimHits.HCalSD.UsePMTHits = cms.bool(False)
     process.g4SimHits.HCalSD.UseFibreBundleHits = cms.bool(False)
-
+ 
     return(process)
